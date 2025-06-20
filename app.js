f5cd0b8658b6296661cbf11acc01295047a80e7f
shade_murderer <fjfhhrjd@gmail.com>
2025-06-20 17:51:40 +0500

    Gizmo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Проверка alert
-alert("Сообщение на русском!");  // Кириллица должна быть видна прямо в коде
+alert("Сообщение на русском!");
 
 // Инициализация Telegram WebApp
 if (window.Telegram?.WebApp) {
@@ -7,10 +7,3 @@ if (window.Telegram?.WebApp) {
     tg.expand();
     tg.MainButton.setText("Продолжить").show();
 }
-
-function adjustViewport() {
-  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-}
-
-window.addEventListener('resize', adjustViewport);
-adjustViewport();
